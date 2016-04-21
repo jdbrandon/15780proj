7cd2a53f76934926b6a3da8fe2743ed6cc9e7d07
keenlooks <keenlooks@gmail.com>
2016-04-21 13:48:35 -0400

    added projection layer. kinda borken

--- rnn.py
+++ rnn.py
@@ -23,11 +23,11 @@ softsign = lambda x : x / (1 + np.absolute(x))
 #code written with extensive help from 
 #http://www.wildml.com/2015/09/recurrent-neural-networks-tutorial-part-2-implementing-a-language-model-rnn-with-python-numpy-and-theano/
 class RNN:
-    def __init__(self, vocab_size = -1, activ_size=100, bptt_max=5, Wg=None,Wi=None,Wf=None,Wo=None,bg=None,bi=None,bf=None,bo=None):
+    def __init__(self, vocab_size = -1, activ_size=100, bptt_max=5, Wg=None,Wi=None,Wf=None,Wo=None,bg=None,bi=None,bf=None,bo=None,U=None,V=None):
         if(Wg is None or Wi is None or Wf is None):
             self.vocab_size = vocab_size
             self.activ_size = activ_size
-            self.concat_size = vocab_size + activ_size
+            self.concat_size = activ_size + activ_size
             self.bptt_max = bptt_max
             np.random.seed(0)
 
@@ -46,7 +46,7 @@ class RNN:
             self.V = np.random.uniform(-np.sqrt(1.0/activ_size),np.sqrt(1.0/activ_size),(self.vocab_size,self.activ_size))
             #self.W = np.random.uniform(-np.sqrt(1.0/activ_size),np.sqrt(1.0/activ_size),(self.activ_size,self.activ_size))
         else:
-            self.vocab_size = Wg.shape[1]-Wg.shape[0]
+            self.vocab_size = U.shape[1]
             self.activ_size = Wg.shape[0]
             self.concat_size = vocab_size + activ_size
             self.bptt_max = bptt_max
@@ -60,6 +60,9 @@ class RNN:
             self.bi = bi
             self.bf = bf
             self.bo = bo
+            
+            self.U = U
+            self.V = V
 
     def rnn_fprop(self, x):
         activ = np.zeros((len(x)+1,self.activ_size))
@@ -77,11 +80,12 @@ class RNN:
         s_prev = np.zeros(self.activ_size)
         
         for t in xrange(len(x)):
-            xc = np.hstack
-            g[t] = np.tanh(self.Wg[:,x[t]] + np.dot(self.Wg[:,-h.shape[1]:],h[t-1]) + self.bg)
-            i[t] = sigmoid(self.Wi[:,x[t]] + np.dot(self.Wi[:,-h.shape[1]:],h[t-1]) + self.bi)
-            f[t] = sigmoid(self.Wf[:,x[t]] + np.dot(self.Wf[:,-h.shape[1]:],h[t-1]) + self.bf)
-            o[t] = sigmoid(self.Wo[:,x[t]] + np.dot(self.Wo[:,-h.shape[1]:],h[t-1]) + self.bo)
+            
+            xc = np.hstack((self.U[:,x[t]],h[t-1]))
+            g[t] = np.tanh(np.dot(self.Wg,xc) + self.bg)
+            i[t] = sigmoid(np.dot(self.Wi,xc) + self.bi)
+            f[t] = sigmoid(np.dot(self.Wf,xc) + self.bf)
+            o[t] = sigmoid(np.dot(self.Wo,xc) + self.bo)
             s[t] = g[t] * i[t] + s[t-1] * f[t]
             h[t] = s[t] * o[t]
         return (g,i,f,o,s,h)
@@ -90,7 +94,7 @@ class RNN:
         loss = 0
         for idx in xrange(len(y)):
             (g,i,f,o,s,h) = self.rnn_fprop(x[idx])
-            output = softmax(np.dot(h,self.Wg)[:,:self.vocab_size])
+            output = softmax(np.dot(self.V, h[-1]))
             how_correct = output[xrange(len(y[idx])),y[idx]]
             loss -= np.sum(np.log(how_correct))
         totalwords = np.sum((len(yi) for yi in y))
@@ -107,8 +111,11 @@ class RNN:
         dbi = np.zeros(self.activ_size)
         dbf = np.zeros(self.activ_size)
         dbo = np.zeros(self.activ_size)
+        
+        dU, dV = np.zeros(self.U.shape),np.zeros(self.V.shape)
+
         diff_s = np.zeros(self.activ_size)
-        output = softmax(np.dot(h,self.Wg[:,:self.vocab_size]))
+        output = softmax(np.dot(self.V,h))
         d_output = output
         d_output[xrange(len(y)),y] -= 1
         d_outputc = np.dot(d_output,self.Wg[:,:self.vocab_size].transpose())
@@ -117,8 +124,11 @@ class RNN:
         bdh = np.zeros(self.activ_size)
         
         for idx in xrange(len(y)-1,-1,-1):
-            ds = o[idx] * ((h[idx]-d_outputc[idx]) + bdh) + diff_s
-            do = s[idx] * ((h[idx] - d_outputc[idx]) + bdh)
+            dV += np.outer(d_output[idx],h[idx])
+            d_t = self.V.transpose().dot(d_output[idx]) * (1 - (h[idx]**2))
+            dU[:,x[t]] += d_t
+            ds = o[idx] * ((h[idx] - d_t) + bdh) + diff_s
+            do = s[idx] * ((h[idx] - d_t) + bdh)
             di = g[idx] * ds
             dg = i[idx] * ds
             df = s[idx-1] * ds
@@ -128,9 +138,7 @@ class RNN:
             do_input = (1. - o[idx]) * o[idx] * do
             dg_input = (1. - g[idx] ** 2) * dg
             
-            onehotx = np.zeros(self.vocab_size)
-            onehotx[x[idx]] = 1
-            xc = np.hstack((onehotx, h[idx-1]))
+            xc = np.hstack((self.U[:,x[t]],h[idx-1]))
             
             dWi += np.outer(di_input, xc)
             dWf += np.outer(df_input, xc)
@@ -148,8 +156,8 @@ class RNN:
             dxc += np.dot(self.Wg.transpose(), dg_input)
             
             diff_s = ds * f[idx]
-            bdx = dxc[:self.vocab_size]
-            bdh = dxc[self.vocab_size:]
+            bdx = dxc[:self.activ_size]
+            bdh = dxc[self.activ_size:]
             
             '''dV += np.outer(d_output[idx], activ[idx])
             d_t = self.V.transpose().dot(d_output[idx]) * (1 - (activ[idx]**2))
@@ -159,7 +167,7 @@ class RNN:
                 a = self.W.transpose().dot(d_t)
                 b = activ[t-1]**2
                 d_t = a * (1 - b)'''
-        return (dWg, dWi, dWf, dWo, dbg, dbi, dbf, dbg, dbo)
+        return (dWg, dWi, dWf, dWo, dbg, dbi, dbf, dbg, dbo, dU, dV)
     
     def rnn_sgd(self, X, y, epochs=20, alpha=0.01, loss_epoch=5):
         lastloss = self.rnn_loss(X,y)+1
@@ -172,7 +180,7 @@ class RNN:
                     print "Adjusting learning rate to "+str(alpha)
                 lastloss = currloss
             for i in xrange(len(y)):
-                dWg, dWi, dWf, dWo, dbg, dbi, dbf, dbg, dbo = self.rnn_bproptt(X[i],y[i])
+                dWg, dWi, dWf, dWo, dbg, dbi, dbf, dbg, dbo, dU, dV = self.rnn_bproptt(X[i],y[i])
                 self.Wg -= alpha*dWg
                 self.Wi -= alpha*dWi
                 self.Wf -= alpha*dWf
@@ -181,4 +189,6 @@ class RNN:
                 self.bi -= alpha*dbi
                 self.bf -= alpha*dbf
                 self.bo -= alpha*dbo
+                self.U -= alpha*dU
+                self.V -= alpha*dV
         return (self.Wg,self.Wi,self.Wf,self.Wo,self.bg,self.bi,self.bf,self.bo)
